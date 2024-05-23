44bf0bdd4b21827ce45172da71e38677da440b00
BuildTools <unconfigured@null.spigotmc.org>
2024-05-22 19:35:11 -0700

    Implement sign-in, and sign-up form

--- components/AuthForm/Auth.tsx
+++ components/AuthForm/Auth.tsx
@@ -9,9 +9,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Form } from "@/components/ui/form";
 import CustomInput from "./CustomInput";
-import { formSchema } from "@/lib/utils";
+import { cn, formSchema } from "@/lib/utils";
 
-const Auth = ({ type }: { type: string }) => {
+const Auth = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -28,11 +28,17 @@ const Auth = ({ type }: { type: string }) => {
     console.log(values);
   };
 
+  const handleFormChange = () => {
+    if (type === "sign-in") setType("sign-up");
+    else setType("sign-in");
+  };
+
   const [user, setUser] = useState(null);
+  const [type, setType] = useState<"sign-in" | "sign-up">("sign-in");
 
   return (
     <section className="auth-form">
-      <div className="flex flex-col gap-5 md:gap-8">
+      <div className="flex w-full flex-col items-center gap-5 md:gap-8">
         <Link href="/" className="flex cursor-pointer items-center gap-1 px-4">
           <Image
             src="/icon/LogoBlack.webp"
@@ -42,36 +48,42 @@ const Auth = ({ type }: { type: string }) => {
             priority
           />
         </Link>
-
-        <div className="flex flex-col gap-1 md:gap-3">
-          <h1 className="text-[24px] font-semibold text-gray-900 lg:text-[36px]">
+        <hr className="w-2/3" />
+        <div className="flex w-10/12 flex-col gap-1 md:gap-3">
+          <h1 className="text-dark_light text-center text-[24px] font-semibold text-gray-900 lg:text-[36px]">
             {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
-            <p className="text-[16px] font-normal text-gray-600">
+            <p className="text-dark_light pb-5 text-[16px] font-normal text-gray-600">
               {user
                 ? "Link your account to get started."
                 : "Please, enter your details."}
             </p>
           </h1>
-          <div className="flex flex-auto">
+          <div className="flex w-full flex-auto">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
+                className="w-full space-y-8"
               >
-                {type === "sign-in" && (
+                {type === "sign-up" && (
                   <>
-                    <CustomInput
-                      control={form.control}
-                      name="firstName"
-                      label="Your name"
-                      nameHolder="example@micamp.com"
-                    />
-                    <CustomInput
-                      control={form.control}
-                      name="lastName"
-                      label="Your last name"
-                      nameHolder="example@micamp.com"
-                    />
+                    <div className="flex justify-between">
+                      <div className="w-32 ">
+                        <CustomInput
+                          control={form.control}
+                          name="firstName"
+                          label="Your name"
+                          nameHolder="example@micamp.com"
+                        />
+                      </div>
+                      <div className="w-32">
+                        <CustomInput
+                          control={form.control}
+                          name="lastName"
+                          label="Your last name"
+                          nameHolder="example@micamp.com"
+                        />
+                      </div>
+                    </div>
                     <CustomInput
                       control={form.control}
                       name="phone"
@@ -96,7 +108,21 @@ const Auth = ({ type }: { type: string }) => {
                   <Button type="submit">
                     {type === "sign-in" ? "Sign in" : "Sign up"}
                   </Button>
-                  <p className="mt-5 text-center">Create Account</p>
+                  <p className="my-5 cursor-pointer text-center">
+                    Forget password
+                  </p>
+                  <hr />
+                  <p className="mt-5 text-center">
+                    {type === "sign-in"
+                      ? "Do not have an account yet?"
+                      : "Already have an account?"}
+                  </p>
+                  <p
+                    className="cursor-pointer text-center text-sky-600"
+                    onClick={handleFormChange}
+                  >
+                    {type === "sign-in" ? "Sign Up" : "Sign In"}
+                  </p>
                 </div>
               </form>
             </Form>
